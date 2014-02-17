44db1fdebcb320127b38e3e8ded878b4fd0e9aa9
root <root@vl85.cs.sunysb.edu>
2014-02-17 14:55:21 -0500

    Implemented Reading and Writing File.

--- hw1/sys_xconcat.c
+++ hw1/sys_xconcat.c
@@ -1,16 +1,148 @@
 #include <linux/linkage.h>
 #include <linux/moduleloader.h>
-
+#include<linux/fs.h>
+#include<asm/uaccess.h>
+#include<linux/vmalloc.h>
+#include<linux/stat.h>
+#include<linux/types.h>
 asmlinkage extern long (*sysptr)(void *arg);
+int wrapfs_read_file(const char *filename,void *buf,int len, int startPos);
+int wrapfs_write_file(const char* filename,void *buf,int len);
+int wrapfs_create_file(const char *filename,int oflags, mode_t mode);
+struct file_details{
+	const char *outfile;
+	const char **infiles;
+	int infile_count;
+	int oflags;
+	mode_t mode;
+	unsigned int flags;
+
+};
 
 asmlinkage long xconcat(void *arg)
 {
 	/* dummy syscall: returns 0 for non null, -EINVAL for NULL */
-	if (arg == NULL)
-		return -EINVAL;
-	else
-		return 0;
+	int i;
+	struct file_details *details=(struct file_details *)arg; 
+	int len=5;
+	int read_bytes=len;
+	int startPos=0;
+	int write_bytes=0;
+	char *buf;
+	int total_bytes_written=0;
+	int total_bytes_read=0;
+	int percent_data_written=0;
+	int total_files_written=0;
+	printk("Creating file %s",details->outfile);
+	wrapfs_create_file(details->outfile,details->oflags,details->mode);
+		
+	for(i=0;i<details->infile_count;i++){
+		printk("Reading file %s\n", details->infiles[i]);
+		startPos=0;
+		read_bytes=len;
+		while(read_bytes==len){
+			buf=vmalloc(len);
+			read_bytes=wrapfs_read_file(details->infiles[i],(void *) buf,len,startPos );
+			printk("%s %d\n",buf,read_bytes);
+			write_bytes=wrapfs_write_file(details->outfile,(void *)buf,len);
+			startPos+=read_bytes;
+			total_bytes_read+=read_bytes;
+			total_bytes_written+=write_bytes;
+			vfree(buf);
+		}
+		total_files_written+=1;
+	}
+
+	
+	if(details->flags==0x00){
+		return total_bytes_written;
+	}else if(details->flags==0x01){
+		return total_files_written;
+	}else if(details->flags==0x02){
+		percent_data_written=(total_bytes_written/total_bytes_read)*100;
+		return percent_data_written;
+	}
 }
+
+int wrapfs_create_file(const char* filename,int oflags,mode_t mode){
+
+	struct file *filp;
+	mm_segment_t oldfs;
+	filp=filp_open(filename,oflags,mode);
+	printk("%s",filename);
+	if(IS_ERR(filp)){
+		printk("wrapfs_write_file error %d\n",(int) PTR_ERR(filp));
+		return -1;
+	}
+	if(!filp){
+		printk("filp is null");
+		return -2;
+	} 		
+	oldfs=get_fs();
+	set_fs(KERNEL_DS);
+	set_fs(oldfs);
+	filp_close(filp,NULL);
+	return 1;
+
+}
+
+
+
+int wrapfs_read_file(const char* filename, void *buf, int len,int startPos){
+	struct file *filp;
+	mm_segment_t oldfs;
+	int bytes;
+	filp=filp_open(filename,O_RDONLY,0);
+	//printk("here");
+	if(IS_ERR(filp)){
+		printk(KERN_ALERT "wrapfs_read_file error %d\n",(int) PTR_ERR(filp));
+		printk("in erro");
+		return -5;
+	}
+	if(!filp){
+		printk("filp is null");
+		return -6;
+	} 		
+	if(!filp->f_op->read)
+		return -2;
+	//printk("i am here");
+	filp->f_pos=startPos;
+	oldfs=get_fs();
+	set_fs(KERNEL_DS);
+	bytes=filp->f_op->read(filp,buf,len,&filp->f_pos);
+	set_fs(oldfs);
+	
+	filp_close(filp,NULL);
+	return bytes;
+}
+
+
+int wrapfs_write_file(const char *filename,void *buf,int len){
+	struct file *filp;
+	mm_segment_t oldfs;
+	int bytes;
+	filp=filp_open(filename,O_APPEND,0);
+	
+	if(IS_ERR(filp)){
+		printk(KERN_ALERT "wrapfs_write_file error %d\n",(int) PTR_ERR(filp));
+		return -5;
+	}
+	if(!filp){
+		printk("filp is null");
+		return -6;
+	} 		
+	if(!filp->f_op->write)
+		return -2;
+	filp->f_pos=0;
+	oldfs=get_fs();
+	set_fs(KERNEL_DS);
+	bytes=filp->f_op->write(filp,buf,len,&filp->f_pos);
+	set_fs(oldfs);
+	
+	filp_close(filp,NULL);
+	return bytes;
+}
+
 
 static int __init init_sys_xconcat(void)
 {
--- hw1/xhw1.c
+++ hw1/xhw1.c
@@ -2,19 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-
+#include <sys/stat.h>
+#include <sys/types.h>
+#include<fcntl.h>
 #define __NR_xconcat	349	/* our private syscall number */
 
+struct file_details{
+	const char *outfile; //name of output file
+	const char **infiles; //array with names of input files
+	unsigned int infile_count; //number of input files in infiles array
+	int oflags;
+	mode_t mode; //open flags to change behavior of syscall
+	unsigned int flags; //speacial flags to change behavior of syscall
+};
+
+struct file_details args;//=malloc(sizeof(struct file_details*));
+//args.infile_count=2;
 int main(int argc, char *argv[])
 {
 	int rc;
-	void *dummy = (void *) atoi(argv[1]);
+	//struct file_details args;
+	//args.infile_count=2;
+	//const char *a[infile_count];
+	//a[0]="file1.txt";
+	//a[1]="file2.txt";
+	args.outfile="/usr/src/hw1-vabhat/hw1/out2.txt";
+	args.infiles=(const char *[]){"/usr/src/hw1-vabhat/hw1/file1.txt","/usr/src/hw1-vabhat/hw1/file2.txt"};
+	args.infile_count=2;	
+	args.mode=0264;
+	args.oflags=O_CREAT|O_EXCL;
+	args.flags=0x02;
+	//int rc;
+	//void *dummy = (void *) atoi(argv[1]);
 
-  	rc = syscall(__NR_xconcat, dummy);
-	if (rc == 0)
-		printf("syscall returned %d\n", rc);
-	else
-		printf("syscall returned %d (errno=%d)\n", rc, errno);
+  	rc = syscall(__NR_xconcat, (void *)&args);
+	//if (rc == 0)
+	printf("syscall returned %d\n", rc);
+	//else
+	//	printf("syscall returned %d (errno=%d)\n", rc, errno);
 
 	exit(rc);
 }
